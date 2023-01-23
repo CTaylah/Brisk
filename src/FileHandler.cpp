bf602d2d60f4f9925b1e1bdd462ba120cc90e2d7
Cardell Taylor <thelemonman22@gmail.com>
2023-01-23 00:11:55 -0500

    initial commit

---
+++ src/FileHandler.cpp
@@ -0,0 +1,25 @@
+#include "FileHandler.h"
+
+FileHandler::FileHandler()
+{
+}
+std::string FileHandler::fileToString(const std::string filePath)
+{
+    std::ifstream inputFileStream;
+    inputFileStream.open(filePath);
+    if (!inputFileStream.is_open())
+    {
+        std::cout << "Error: Could not open file \n" + filePath << std::endl;
+    }
+
+    std::string outputString;
+    std::string line;
+    while (std::getline(inputFileStream, line))
+    {
+        outputString = outputString + line + "\n";
+    }
+
+    inputFileStream.close();
+
+    return outputString;
+}
